1072d6b86a290c1a088fe99d9bb6ea3a8ee392d0
taras <taras.batenkov@gmail.com>
2018-07-06 18:44:52 +0300

    init

---
+++ config/passport-setup.js
@@ -0,0 +1,13 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20')
+
+passport.use(
+    new GoogleStrategy({
+        // options
+        callbackURL: '/auth/google/redirect',
+        clientID: '740637716139-5fchdi057fpcevd4v9dinsgpub5j5iop.apps.googleusercontent.com',
+        clientSecret: '-kvx1U5uWLIvm16jJlvA-DBR',
+    }, () => {
+        // passport cb
+    })
+)
