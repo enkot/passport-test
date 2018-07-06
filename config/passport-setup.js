830629a583379e8f0a73a07bb5300119d6b9a8b9
taras <taras.batenkov@gmail.com>
2018-07-06 18:55:23 +0300

    got google auth token

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -7,7 +7,7 @@ passport.use(
         callbackURL: '/auth/google/redirect',
         clientID: '740637716139-5fchdi057fpcevd4v9dinsgpub5j5iop.apps.googleusercontent.com',
         clientSecret: '-kvx1U5uWLIvm16jJlvA-DBR',
-    }, () => {
+    }, (accessToken, refreshToken, profile, done) => {
         // passport cb
     })
 )
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -16,4 +16,9 @@ router.get('/google', passport.authenticate('google', {
     scope: ['profile']
 }))
 
+// callback route for google
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+    res.send('you reached callback uri')
+})
+
 module.exports = router
